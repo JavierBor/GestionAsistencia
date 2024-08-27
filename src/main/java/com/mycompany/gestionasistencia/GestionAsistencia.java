55b839652b126d47dc8eb970fbfb3be9f45eb8d6
jborq <jborq@DESKTOP-UUV0BVT>
2024-08-27 11:45:09 -0400

    Creación menú

--- src/main/java/com/mycompany/gestionasistencia/GestionAsistencia.java
+++ src/main/java/com/mycompany/gestionasistencia/GestionAsistencia.java
@@ -1,16 +1,30 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
-
 package com.mycompany.gestionasistencia;
-
-/**
- *
- * @author jborq
- */
+import java.io.*;
 public class GestionAsistencia {
 
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+    public static void main(String[] args)throws IOException{
+        BufferedReader lector = new BufferedReader(new InputStreamReader(System.in));
+        String opcion;
+        do{
+            System.out.println("1. Pasar asistencia.");
+            System.out.println("2. Modificar asistencia.");
+            System.out.println("3. Mostrar registros");
+            System.out.println("4. Salir");
+            System.out.print("Ingrese una opción: ");
+            opcion = lector.readLine();
+            switch (opcion) {
+                case "1" -> System.out.println("Pasar lista");
+                case "2" -> {
+                    System.out.println("Modificando asistencia");
+                    System.out.print("Ingrese nombre del alumno:");
+                    System.out.println();
+                }
+                case "3" -> System.out.println("Mostrando registros");
+                case "4" -> {
+                    System.out.println("Saliendo del programa");
+                }
+
+            }
+        }while (!opcion.equals("4"));
     }
 }

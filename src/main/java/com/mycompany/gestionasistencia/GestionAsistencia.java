ebfe0773603b58b5e3a5e554b57a6bb5d032af4b
Diego Valenzuela <>
2024-08-28 15:47:46 -0400

    Intento de commit

--- src/main/java/com/mycompany/gestionasistencia/GestionAsistencia.java
+++ src/main/java/com/mycompany/gestionasistencia/GestionAsistencia.java
@@ -1,30 +1,33 @@
+  
 package com.mycompany.gestionasistencia;
 import java.io.*;
+
 public class GestionAsistencia {
 
     public static void main(String[] args)throws IOException{
         BufferedReader lector = new BufferedReader(new InputStreamReader(System.in));
-        String opcion;
+        int opcion;
         do{
             System.out.println("1. Pasar asistencia.");
             System.out.println("2. Modificar asistencia.");
             System.out.println("3. Mostrar registros");
             System.out.println("4. Salir");
             System.out.print("Ingrese una opción: ");
-            opcion = lector.readLine();
-            switch (opcion) {
-                case "1" -> System.out.println("Pasar lista");
-                case "2" -> {
+            opcion = Integer.parseInt(lector.readLine());
+            switch (opcion) 
+            {
+                case 1: 
+                    System.out.println("Pasar lista");
+                    break;
+                case 2:
                     System.out.println("Modificando asistencia");
                     System.out.print("Ingrese nombre del alumno:");
                     System.out.println();
-                }
-                case "3" -> System.out.println("Mostrando registros");
-                case "4" -> {
-                    System.out.println("Saliendo del programa");
-                }
-
+                    break;
+                case 3:
+                    System.out.println("Mostrando registros");
             }
-        }while (!opcion.equals("4"));
+        } while (opcion != 4);
+        System.out.println("Saliendo del programa...");
     }
 }

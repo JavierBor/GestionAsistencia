d49d68969ed3b174bad41e3f902d314efe5d193b
jborq <jborq@DESKTOP-UUV0BVT>
2024-10-01 19:24:01 -0300

    AlumnoNoEncontradoEception

---
+++ src/main/java/com/mycompany/gestionasistencia/AlumnoNoEncontradoException.java
@@ -0,0 +1,7 @@
+package com.mycompany.gestionasistencia;
+
+public class AlumnoNoEncontradoException extends Exception{
+    public AlumnoNoEncontradoException(String mensaje){
+        super(mensaje);
+    }
+}
